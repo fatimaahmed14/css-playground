562513530a9ebada02f6a8d31995fb0e2f481086
Fatima <fxtimaahmed14@gmail.com>
2022-12-30 13:55:37 +0000

    more styling changes

--- css-playground/src/components/StartPage.js
+++ css-playground/src/components/StartPage.js
@@ -4,7 +4,9 @@ function StartPage() {
   return (
     <>
       <div className="startpage">
-        <header className="header">This is the header</header>
+        <header className="header">
+          <div className="title">Fatima's CSS Playground</div>
+        </header>
         <main className="main">
           <div className="box"></div>
           <div className="box"></div>
